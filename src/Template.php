ba7f231980f6489a0b34dfcbf5f2681632dc93ff
protonigramer <55741205+protonigramer@users.noreply.github.com>
2019-12-03 13:26:07 +0100

    Expand the template class

--- src/Template.php
+++ src/Template.php
@@ -4,11 +4,21 @@ namespace Weissheiten\Sassxample;
 
 class Template{
 
-    public function render(){
-        $homepage = file_get_contents('./src/Templates/home.html');
-        $homepage = str_replace("###username###",'WSF',$homepage);
-        echo $homepage;
+    /**
+     * @param String $templatePath The string path of the HTML-template.
+     * @param array $templateValues An associative array consisting values which should be replaced
+     * @param String $prefix character sequence indicating the begin of values
+     * @param String $end character sequence indicating the end of values
+     * @return String The HTML markup after templating
+     */
+    public function render(String $templatePath, array $templateValues, String $prefix = "###", String $end = '###') : String{
+        $homepage = file_get_contents($templatePath);
 
+        foreach ($templateValues as $key => $value) {
+            $homepage = str_replace($prefix.$key.$end,$value,$homepage);
+        }
+
+        return $homepage;
     }
 
 }
